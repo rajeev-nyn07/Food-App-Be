f3a94f119c23971e386a7d0efa42974d30516254
nineleaps <rajeev.nayan@nineleaps.com>
2023-03-24 10:55:43 +0530

    1stPush

---
+++ src/main/java/com/nineleaps/foodtechapp/FoodtechappApplication.java
@@ -0,0 +1,13 @@
+package com.nineleaps.foodtechapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodtechappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodtechappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nineleaps/foodtechapp/FoodtechappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nineleaps.foodtechapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodtechappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
